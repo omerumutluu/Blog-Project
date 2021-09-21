1e443fdcd7ea18ec5f84bc1b44393f6def6bdb47
Omer Umutlu <203531012@uzem.education>
2021-09-22 00:47:00 +0300

    Add project files.

---
+++ AspNetCoreProjeKampı/Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProjeKampı.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Add(blog);
+            context.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Remove(blog);
+            context.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var context = new Context();
+            return context.Blogs.Find(id);
+        }
+
+        public List<Blog> ListAllCategory()
+        {
+            using var context = new Context();
+            return context.Blogs.ToList();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Update(blog);
+            context.SaveChanges();
+        }
+    }
+}
